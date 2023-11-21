e97d31ce715eddc9694385e20e61757cd0e3654b
Mechanist <dylanjcooksley@gmail.com>
2023-11-21 16:20:28 -0600

    Error Handle for failed auth

--- Assignment4/Home.aspx.cs
+++ Assignment4/Home.aspx.cs
@@ -36,46 +36,49 @@ namespace Assignment4.webpages
             HttpContext.Current.Session["uPass"] = nPassword;
 
 
+            try {
+                // Search for the current User, validate UserName and Password
+                NetUser myUser = (from x in dbcon.NetUsers
+                                  where x.UserName == HttpContext.Current.Session["nUserName"].ToString()
+                                        && x.UserPassword == HttpContext.Current.Session["uPass"].ToString()
+                                  select x).First();
 
-            // Search for the current User, validate UserName and Password
-            NetUser myUser = (from x in dbcon.NetUsers
-                              where x.UserName == HttpContext.Current.Session["nUserName"].ToString()
-                                    && x.UserPassword == HttpContext.Current.Session["uPass"].ToString()
-                                    select x).First();
+                if (myUser != null)
+                {
+                    //Add UserID and User type to the Session
+                    HttpContext.Current.Session["userID"] = myUser.UserID;
+                    HttpContext.Current.Session["userType"] = myUser.UserType;
 
-            if (myUser != null)
-            {
-                //Add UserID and User type to the Session
-                HttpContext.Current.Session["userID"] = myUser.UserID;
-                HttpContext.Current.Session["userType"] = myUser.UserType;
+                }
+                if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Member")
+                {
 
+                    FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
+
+                    Response.Redirect("~/Member/member.aspx");
+                }
+                else if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Instructor")
+                {
+
+                    FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
+
+                    Response.Redirect("~/Instructor/instructor.aspx");
+                }
+                else if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Administrator")
+                {
+
+                    FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
+
+                    Response.Redirect("~/Admin/admin.aspx");
+                }
+                else
+                    Response.Redirect("Logon.aspx", true);
+
+                } catch (Exception ex) 
+                {
+                    Login2.FailureText = "Failed to Authenticate" + ex.Message;
+                }
             }
-            if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Member")
-            {
-
-                FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
-
-                Response.Redirect("~/Member/member.aspx");
-            }
-            else if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Instructor")
-            {
-
-                FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
-
-                Response.Redirect("~/Instructor/instructor.aspx");
-            }
-            else if (myUser != null && HttpContext.Current.Session["userType"].ToString().Trim() == "Administrator")
-            {
-
-                FormsAuthentication.RedirectFromLoginPage(HttpContext.Current.Session["nUserName"].ToString(), true);
-
-                Response.Redirect("~/Admin/admin.aspx");
-            }
-            else
-                Response.Redirect("Logon.aspx", true);
-
-
-        }
     }
 
 }
